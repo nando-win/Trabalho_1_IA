b451cd13be8f6f56c3593bebbda3fcb03bb000be
Fernando Winston Cavalcante Bezerra <fernando.wcb@gmail.com>
2020-05-09 20:04:42 -0300

    fix py

--- Trabalho_1/distancias.py
+++ Trabalho_1/distancias.py
@@ -1,9 +1,12 @@
-import csv
+# import csv
 
-# read flash.dat to a list of lists
-datContent = [i.strip().split() for i in open("./iris_log.dat").readlines()]
+import numpy as np
 
-# write it as a new CSV file
-with open("./iris_log.csv", "wb") as f:
-    writer = csv.writer(f)
-    writer.writerows(datContent)
+
+data = np.genfromtxt('iris_log.dat',
+                     encoding='ASCII',
+                     names=True,
+                     dtype=None,
+                     delimiter=' ')
+
+# print(data)
