583e53edbece2437c4a846e21970095bfadddfd4
Fernando Winston Cavalcante Bezerra <fernando.wcb@gmail.com>
2020-05-09 20:21:38 -0300

    trying read values

--- Trabalho_1/distancias.py
+++ Trabalho_1/distancias.py
@@ -4,9 +4,24 @@ import numpy as np
 
 
 data = np.genfromtxt('iris_log.dat',
-                     encoding='ASCII',
                      names=True,
                      dtype=None,
                      delimiter=' ')
 
-# print(data)
+class1 = data[0:5]
+class2 = data[5:10]
+class3 = data[10:15]
+
+class1_center = class1.mean(axis=0)[0:3]
+class2_center = class2.mean(axis=0)[0:3]
+class3_center = class3.mean(axis=0)[0:3]
+
+print(data)
+
+len_dta = data.shape[0]
+
+for i in range(0, len_dta):
+    
+    distances = np.array([np.sqrt(((class1_center - data[i][0:3])**2).sum()),
+                          np.sqrt(((class2_center - data[i][0:3])**2).sum()),
+                          np.sqrt(((class3_center - data[i][0:3])**2).sum())])
